c18cd4c5c7ab841ea0f83a8c7eac9080ec7d49de
Vordty <tokomostwanted@gmail.com>
2021-07-08 13:16:17 +0400

    Modify writeImage option prop names

--- src/helpers/writeImage.js
+++ src/helpers/writeImage.js
@@ -1,15 +1,15 @@
 export const writeImage = async (image, index, ffmpeg, options) => {
 	console.log("writeImage options", options);
-	const { IMG_PREFIX = "frame", IMG_TYPE = "png" } = options;
+	const { prefix = "frame", type = "png" } = options;
 
 	if (!ffmpeg.isLoaded()) await ffmpeg.load();
 
 	const arrayBuffer = convertDataURIToBinary(image);
 
-	const filename = `${IMG_PREFIX}-${index.toString().padStart(3, "0")}.${IMG_TYPE}`;
+	const filename = `${prefix}-${index.toString().padStart(3, "0")}.${type}`;
 	await ffmpeg.FS(
 		"writeFile",
-		`${IMG_PREFIX}-${index.toString().padStart(3, "0")}.${IMG_TYPE}`,
+		`${prefix}-${index.toString().padStart(3, "0")}.${type}`,
 		arrayBuffer,
 	);
 
@@ -18,7 +18,7 @@ export const writeImage = async (image, index, ffmpeg, options) => {
 
 const BASE64_MARKER = ";base64,";
 
-export const convertDataURIToBinary = (dataURI) => {
+const convertDataURIToBinary = (dataURI) => {
 	const base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
 	const base64 = dataURI.substring(base64Index);
 	const raw = window.atob(base64);
