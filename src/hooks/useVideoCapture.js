de8b58d88bc2d190e25e390dd220ee8b60d5245e
Vordty <tokomostwanted@gmail.com>
2021-07-08 13:16:58 +0400

    Integrate video export in useVideoCapture hook

--- src/hooks/useVideoCapture.js
+++ src/hooks/useVideoCapture.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import { takeScreenshot } from "../helpers/takeScreenshot";
 import { writeImage } from "../helpers/writeImage";
+import { generateVideo } from "../helpers/generateVideo";
 
 import { createFFmpeg } from "@ffmpeg/ffmpeg";
 
@@ -17,7 +18,7 @@ export const useVideoCapture = (node, trigger, options) => {
 				corePath: "https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js",
 				log: true,
 			});
-			await ffmpeg.load();
+			if (!ffmpeg?.isLoaded()) await ffmpeg.load();
 		};
 
 		setupFFMPEG();
@@ -35,9 +36,24 @@ export const useVideoCapture = (node, trigger, options) => {
 		const image = await takeScreenshot(node, {
 			html2canvas: { proxy: "http://localhost:5000/" },
 		});
-		setFrames((frames) => [...frames, image]);
 		const imageFilePath = await writeImage(image, frames.length, ffmpeg, {});
+		setFrames((frames) => [
+			...frames,
+			{
+				src: image,
+				path: imageFilePath,
+			},
+		]);
 		console.log("imageFilePath", imageFilePath);
+	};
+
+	const exportVideo = () => {
+		stopWatching();
+		generateVideo(
+			ffmpeg,
+			frames.map((frame) => frame.path),
+			{},
+		);
 	};
 
 	const startWatching = () => {
@@ -52,5 +68,7 @@ export const useVideoCapture = (node, trigger, options) => {
 		isWatching,
 		startWatching,
 		stopWatching,
+		addFrame,
+		exportVideo,
 	};
 };
