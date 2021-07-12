4bfd1afba6d3a14f9900931608bc7a8483d7e696
Vordty <tokomostwanted@gmail.com>
2021-07-12 13:17:45 +0400

    Add default options for useVideoCapture hook

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const DEFAULT_OPTIONS = {
+	frame: {
+		filename: "frame",
+		filetype: "png",
+		mime: "image/png",
+		quality: 1,
+	},
+	html2canvas: {},
+	video: {
+		framerate: "1",
+		filename: "video",
+		filetype: "webm",
+		mime: "video/webm",
+	},
+};
